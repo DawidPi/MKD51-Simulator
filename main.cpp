7f6b42de8b442862e97dd79a63d37a9cd25d0ebe
Dawid <pilarski.dawid94@gmail.com>
2015-04-08 02:26:09 +0200

    minor fixes

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
 #include <QApplication>
 #include "View/headers/mainview.h"
 #include <thread>
-
-
-void main1(int m_argc, char** m_argv) {
-    View::MainView* m_view;
-    QApplication App(m_argc, m_argv);
-    qApp->setStyleSheet("QGroupBox {"
-                        "border: 1px solid gray;"
-                        "border-radius: 3px;"
-                        "margin-top: 0.5em;"
-                        "}"
-
-                        "QGroupBox::title {"
-                        "subcontrol-origin: margin;"
-                        "left: 10px;"
-                        "padding: 0 3px 0 3px;"
-                        "}"
-                        );
-
-    m_view = new View::MainView;
-    m_view->show();
-
-    App.exec();
-}
+#include "Controller/headers/simulator.h"
 
 int main(int argc, char *argv[])
 {
